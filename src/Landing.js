edf384cc904274a48308798d031fc0225a13e9a3
Benjamin Herson <benjaminherson@gmail.com>
2017-08-12 14:38:05 -0700

    change Landing activity choice to Radio buttons, add functionality to hide survey question

--- src/Landing.js
+++ src/Landing.js
@@ -1,36 +1,46 @@
 import React, { Component } from 'react';
+import { FormGroup, InputGroup, Radio} from 'react-bootstrap';
 
 class Landing extends Component {
+  componentWillMount() {
+    this.setState({"isSurveyVisible": true});
+  }
+  
   render() {
     const btnTxt = "Get My Number";
+    let surveyClass = this.state.isSurveyVisible ? "" : "extraForm";
     return (
       <div>
         <h3>Epicodus Alumni</h3>
         <h5>Get Your Raffle Ticket</h5>
-        <form>
-          <div>
+        <FormGroup>
+          <InputGroup>
             <label>Name</label>
             <input type="text" required="required" />
-          </div>
-          <div>
-            <p>
-              <input name="useCase" type="radio" checked/>
-              I Want To Answer A Survey Question
-            </p>
-            <p>
-              <input name="useCase" type="radio" />
-              I Just Need To Get My Ticket Number
-            </p>
-          </div>
-          <div>
+          </InputGroup>
+          
+          <Radio onClick={this.toggleSurvey.bind(this, true)} defaultChecked>
+            I Want To Answer A Survey question
+          </Radio>
+          <Radio onClick={this.toggleSurvey.bind(this, false)}>
+            I Just Need To Get My Ticket Number
+          </Radio>
+          
+          <InputGroup className={surveyClass}>
             <label>{this.props.question}</label>
             <textarea rows="5" cols="30" maxLength="1000" required="required"/>
-          </div>
+          </InputGroup>
           <button>{btnTxt}</button>
-        </form>
+        </FormGroup>
       </div>
     )
   }
+  
+  toggleSurvey(surveyState) {  
+    this.setState({"isSurveyVisible": surveyState});
+  }
+  
+  
 }
 
 export default Landing;
