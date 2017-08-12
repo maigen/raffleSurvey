cdd2fb208f46af9dbcd20481c6344107a77bf9a8
Benjamin Herson <benjaminherson@gmail.com>
2017-08-12 16:31:29 -0700

    add functionality to record name into state, add notes for firebase interactions

--- src/Landing.js
+++ src/Landing.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
-import { FormGroup, InputGroup, Radio} from 'react-bootstrap';
+import {
+  FormControl,
+  InputGroup,
+  Radio
+} from 'react-bootstrap';
 
 class Landing extends Component {
   componentWillMount() {
-    this.setState({"isSurveyVisible": true});
+    this.setState({
+      "isSurveyVisible": true,
+      "name": ''
+    });
   }
   
   render() {
-    const btnTxt = "Get My Number";
+    const btnTxt = this.state.isSurveyVisible ? "Submit" : "Grab Number";
     let surveyClass = this.state.isSurveyVisible ? "" : "extraForm";
     return (
       <div>
         <h3>Epicodus Alumni</h3>
         <h5>Get Your Raffle Ticket</h5>
-        <FormGroup>
-          <InputGroup>
-            <label>Name</label>
-            <input type="text" required="required" />
-          </InputGroup>
+        <div>
+          <FormControl
+            type="text"
+            label="name"
+            onChange={this.recordInfo.bind(this, "name")}
+          />
           
           <Radio onClick={this.toggleSurvey.bind(this, true)} defaultChecked>
             I Want To Answer A Survey question
@@ -30,14 +38,37 @@ class Landing extends Component {
             <label>{this.props.question}</label>
             <textarea rows="5" cols="30" maxLength="1000" required="required"/>
           </InputGroup>
-          <button>{btnTxt}</button>
-        </FormGroup>
+          <button onClick={this.sendRequest.bind(this)}>{btnTxt}</button>
+        </div>
       </div>
     )
   }
   
+  recordInfo(key, e) {
+    this.setState({ [key]: e.target.value });
+  }
+  
   toggleSurvey(surveyState) {  
     this.setState({"isSurveyVisible": surveyState});
+  }
+  
+  sendRequest() {
+    if (this.state.name === "") {
+      return;
+    }
+    
+    if (this.state.isSurveyVisible) {
+      console.log(this.state.name);
+      // put survey answer into appropriate array
+      // get the next ticket number and assign it to name in entrants
+      // push ticket number into entries
+      // make sure next user will get a new ticket number
+    } else {
+      // verify name exists in FB
+        //(what if not???)
+      // get ticket number for name
+    }
+    // route to Ticket with ticket number
   }
   
   
