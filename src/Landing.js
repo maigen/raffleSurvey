be7b9855de4d2ef24be351878a51d35465e5589c
Jenny Olsen <jenny.a.olsen@gmail.com>
2017-08-13 12:10:01 -0700

    question form layout

--- src/Landing.js
+++ src/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FormControl, Radio } from 'react-bootstrap';
+import { FormControl, ControlLabel, Radio } from 'react-bootstrap';
 
 class Landing extends Component {
   componentWillMount() {
@@ -9,7 +9,7 @@ class Landing extends Component {
       "name": ''
     });
   }
-  
+
   render() {
     const btnTxt = this.state.isSurveyVisible ? "Submit" : "Grab Number";
     let surveyClass = this.state.isSurveyVisible ? "" : "extraForm";
@@ -17,25 +17,25 @@ class Landing extends Component {
       <div>
         <h3>Epicodus Alumni</h3>
         <h5>Get Your Raffle Ticket</h5>
-        <div>
+        <div className="question-form">
+        <ControlLabel>Name: </ControlLabel>
           <FormControl
             type="text"
-            label="name"
             onChange={this.recordInfo.bind(this, "name")}
           />
-          
+
           <Radio name="useCase" onClick={this.toggleSurvey.bind(this, true)} defaultChecked>
             I Want To Answer A Survey question
           </Radio>
           <Radio name="useCase" onClick={this.toggleSurvey.bind(this, false)}>
             I Just Need To Get My Ticket Number
           </Radio>
-          
+
+          <ControlLabel>{this.props.question}</ControlLabel>
           <FormControl
             className={surveyClass}
             componentClass="textarea"
             type="text"
-            label={this.props.question}
             onChange={this.recordInfo.bind(this, "answer")}
           />
           <button onClick={this.sendRequest.bind(this)}>{btnTxt}</button>
@@ -43,20 +43,20 @@ class Landing extends Component {
       </div>
     )
   }
-  
+
   recordInfo(key, e) {
     this.setState({ [key]: e.target.value });
   }
-  
-  toggleSurvey(surveyState) {  
+
+  toggleSurvey(surveyState) {
     this.setState({"isSurveyVisible": surveyState});
   }
-  
+
   sendRequest() {
     if (this.state.name === "") {
       return;
     }
-    
+
     if (this.state.isSurveyVisible && this.state.answer !== "") {
       console.log(this.state.name);
       console.log(this.props.question);
@@ -71,11 +71,11 @@ class Landing extends Component {
       // get ticket number for name
     }
     // route to Ticket with ticket number
-    
+
     // rearrange cases to handle validation possibilities, maybe pop a message
   }
-  
-  
+
+
 }
 
 export default Landing;
