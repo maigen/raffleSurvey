f3a52aedff000db9f51ed242e509e84c1e08163c
Benjamin Herson <benjaminherson@gmail.com>
2018-08-22 22:27:07 -0700

    add ability to retrieve entries and select a winner or winners (without duplicates)

--- src/AmbIn.js
+++ src/AmbIn.js
@@ -1,7 +1,16 @@
+/*global firebase*/
 import React, { Component } from 'react';
 import { Tabs, Tab } from 'react-bootstrap';
 
 class AmbIn extends Component {
+  componentWillMount() {
+    this.setState({
+      entries: [],
+      winners: [],
+      displayData: { ticket: '', name: ''}
+    });
+  }
+  
   render() {
     const tabs = [];
     
@@ -12,10 +21,15 @@ class AmbIn extends Component {
     return (
       <div>
         <h3>Amber's Admin</h3>
-        <button>Draw A Winning Number</button>
+        <button onClick={this.selectWinner.bind(this)}>Draw A Winning Number</button>
+        
+        <div className="winners">
+          <h4>{this.state.displayData.ticket}</h4>
+          <p>{this.state.displayData.name}</p>
+        </div>
         
         <div>
-          <label>Get Responses</label>
+          {/* <label>Get Responses</label> */}
           <Tabs id="surveyQuestions">
             {tabs}
           </Tabs>
@@ -23,6 +37,56 @@ class AmbIn extends Component {
         </div>
       </div>
     )
+  }
+  
+  selectWinner() {
+    let entries = this.state.entries;
+    let winners = this.state.winners;
+    let winner = '';
+    let needWinner = true;
+    const ender = {name: 'All tickets have been drawn.', ticket: 'Sorry!'};
+        
+    // for when data already retrieved from fb
+    if (entries.length) {  
+      // first check if all tickets drawn
+      if (winners.length === entries.length) {
+        needWinner = false;
+        this.setState({
+          displayData: ender
+        });
+      }
+      
+      // then attempt to draw random ticket
+      while (needWinner) {
+        winner = entries[Math.floor(Math.random() * entries.length)];
+        if (winners.indexOf(winner.ticket) === -1) {
+          //winner is valid as a NEW winner
+          needWinner = false;
+          winners.push(winner.ticket);
+          console.log(winners);
+          this.setState({
+            winners,
+            displayData: winner
+          });
+        }
+      }
+    } else {
+      // first retrieval only
+      firebase.database().ref('entries').once('value').then(snapshot => {
+        const entryData = snapshot.val();
+        entries = Object.keys(entryData).reduce((acc, key) => {
+          const item = entryData[key];
+          acc.push({name: item.name, ticket: item.ticket});
+          return acc;
+        }, []);
+        winner = entries[Math.floor(Math.random() * entries.length)];
+        this.setState({
+          entries,
+          winners: [winner.ticket],
+          displayData: winner
+        });
+      });
+    }
   }
 }
 
